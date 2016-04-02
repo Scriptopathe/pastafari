d78814e8333ab0227229a973a67cfea0ad73a838
gautierenaud <gautierenaud@gmail.com>
2016-04-02 21:19:45 +0200

    start implementing ia city

---
+++ src/ia/IACity.java
@@ -0,0 +1,21 @@
+package ia;
+
+import java.util.ArrayList;
+
+import pastafari.GameServer;
+import pastafari.GameState;
+import pastafari.Player;
+import pastafari.units.Unit;
+
+public class IACity implements IAInterface{
+
+	@Override
+	public void makeTurn(GameServer srv) {
+		/*GameState state = srv.getGameState();
+		int myID = state.getMyId();
+		Player pMe = state.getPlayer(myID);
+		while (pMe.getGold() > 10){
+			
+		}*/
+	}
+}
