178f74bd4c635ddd6b8fd5bb496d6c0fe3982a6f
Scriptopathe <josue.alvarez@hotmail.fr>
2016-04-02 16:47:44 +0200

    Ajout du game server

---
+++ src/ia/IAInterface.java
@@ -0,0 +1,8 @@
+package ia;
+
+import pastafari.GameServer;
+
+public interface IAInterface {
+	
+	public void makeTurn(GameServer srv);
+}
--- src/pastafari/Main.java
+++ src/pastafari/Main.java
@@ -1,10 +1,29 @@
 package pastafari;
 
+import ia.IAInterface;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		new GameServer("127.0.0.1", 8080, new IAInterface() {
+			
+			@Override
+			public void makeTurn(GameServer srv) {
+				// TODO Auto-generated method stub
+				
+			}
+		}).start();
+		
+		new GameServer("127.0.0.1", 8080, new IAInterface() {
+			
+			@Override
+			public void makeTurn(GameServer srv) {
+				// TODO Auto-generated method stub
+				
+			}
+		}).start();
 	}
 
 }
