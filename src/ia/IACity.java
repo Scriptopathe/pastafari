09c58b53a6aa1452f2d5d6907397aa89b4e4338f
gautierenaud <gautierenaud@gmail.com>
2016-04-02 21:34:38 +0200

    src/ia/IACity.java

--- src/ia/IACity.java
+++ src/ia/IACity.java
@@ -11,11 +11,18 @@ public class IACity implements IAInterface{
 
 	@Override
 	public void makeTurn(GameServer srv) {
-		/*GameState state = srv.getGameState();
+		GameState state = srv.getGameState();
 		int myID = state.getMyId();
 		Player pMe = state.getPlayer(myID);
-		while (pMe.getGold() > 10){
-			
-		}*/
+		int newPlace[] = pMe.getCity().leftPlace(state.getGrid());
+		/*
+		while (pMe.getGold() > 10 && newPlace != null){ // si on a la place et l'argent
+			srv.sendCommand("C,P");
+			pMe.setGold(pMe.getGold() - 10);
+			srv.sendCommand(command)
+			newPlace = pMe.getCity().leftPlace(state.getGrid());
+		}
+		*/
+		srv.sendCommand("C,P");
 	}
 }
