0fd2c9eeeba48d6ca1cc3c3c628c1caff0cb4b31
graphman65 <graphman65@gmail.com>
2016-04-02 23:55:42 +0200

    Cleanup

--- src/pastafari/Grid.java
+++ src/pastafari/Grid.java
@@ -1,6 +1,8 @@
 package pastafari;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.stream.Stream;
 
 import pastafari.structures.Building;
 import pastafari.structures.BuildingType;
@@ -43,10 +45,7 @@ public class Grid {
 	public void setTile(int x, int y, Tile tile) {
 		this.tiles[x][y] = tile;
 	}
-	
-	public void getMovingCost(Tile from, Tile to) {
-		return;
-	}
+
 	
 	public static int getDistance(Tile from, Tile to) {
 		return Math.max(Math.abs(from.getX()-to.getX()), Math.abs(from.getY()-to.getY()));
