f2303c3ad091e59abd560aeffa5342404c014359
Pro1d <topaffaire_pg@hotmail.fr>
2016-04-03 00:00:03 +0200

    Fixes and swag

--- src/pastafari/GameState.java
+++ src/pastafari/GameState.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 public class GameState {
 	private HashMap<Integer, Player> players;
 	private Grid grid;
-	private int size;
 	private int myId;
 
 	public GameState(int size, int myId) {
@@ -30,10 +29,6 @@ public class GameState {
 	
 	public Player getPlayer(int id) {
 		return this.players.get(id);
-	}
-	
-	public int getSize() {
-		return size;
 	}
 	
 	public Player getMyPlayer() {
