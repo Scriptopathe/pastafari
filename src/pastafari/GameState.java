ddbca8bdbba473c5d17cddfb6e4e0b235c73c7c3
graphman65 <graphman65@gmail.com>
2016-04-02 18:44:35 +0200

    Added isMe

--- src/pastafari/GameState.java
+++ src/pastafari/GameState.java
@@ -5,10 +5,14 @@ import java.util.HashMap;
 public class GameState {
 	private HashMap<Integer, Player> players;
 	private Grid grid;
+	private int size;
+	private int id;
 	
-	public GameState(int width, int height) {
+	public GameState(int size, int myID) {
 		this.players = new HashMap<>();
-		this.grid = new Grid(width, height);
+		this.grid = new Grid(size, size);
+		this.size = size;
+		this.id = myID;
 	}
 	
 	public Grid getGrid() {
@@ -21,5 +25,13 @@ public class GameState {
 	
 	public Player getPlayer(int id) {
 		return this.players.get(id);
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public boolean isMe(Player player) {
+		return player.getId() == this.id;
 	}
 }
